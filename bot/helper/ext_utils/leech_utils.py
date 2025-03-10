d275588509d10418d4c6afc5ed053aa74612a505
AS TG <126592884+TGxIRFAN@users.noreply.github.com>
2025-03-10 23:12:19 +0530

    Update leech_utils.py

--- bot/helper/ext_utils/leech_utils.py
+++ bot/helper/ext_utils/leech_utils.py
@@ -252,8 +252,8 @@ async def format_filename(file_, user_id, dirpath=None, isMirror=False):
     lmetadata = user_dict.get('lmetadata', '')
     
     prefile_ = file_
-    file_ = re_sub(r'(www|wwww|ww)\.[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(?:\s?-?)?|(-?\s?1Tamil(MV|Blasters)\.[a-z]{2,5})', '', file_)
-    file_ = re_sub(r'\s+(\.[a-zA-Z0-9]+)', r'\1', file_)
+    file_ = re_sub(r'(www|wwww|ww|w)\.[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(?:\s?-?)?|(-?\s?1Tamil(MV|Blasters)\.[a-z]{2,5})', '', file_)
+    file_ = re_sub(r'^[\s_]+|[-\s]+$|\s+(\.[a-zA-Z0-9]+)', r'\1', file_)
 
     if lmetadata and dirpath and file_.lower().endswith('.mkv'):
         file_ = await change_metadata(file_, dirpath, lmetadata)
